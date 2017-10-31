d18d5c837abb449f51b0141ce7c7b342281091bb
Isaac <isaac.newt.list@icloud.com>
2017-10-30 22:46:22 -0500

    Update Window.py

--- Window.py
+++ Window.py
@@ -3,7 +3,19 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-window = Gtk.Window()
+class MyWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("Hello World")
+
+window = MyWindow()
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()
 
