4b6803d25bbef0ecf2228ff32055c751b5b88936
Isaac <isaac.newt.list@icloud.com>
2017-10-31 21:26:10 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# This program is my first python program!
+
+print('Hello World')
+print('What\'s your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+
+print('Your name is')
+print(len(myName))
+print('characters long')
+
+print('How old are you?')
+myAge = input()
+print('You\'ll be ' + str(int(myAge) + 1) + ' in a year')
