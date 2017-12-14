81a0c23d92583bfe4a275a81205f6b98d41de13e
isaac-newt <isaac.newt.list@icloud.com>
2017-12-13 20:03:11 -0600

    add some examples, comments

--- Scripts/AutomateTheBoringStuff/is_phone_number.py
+++ Scripts/AutomateTheBoringStuff/is_phone_number.py
@@ -16,7 +16,20 @@ def is_phone_number(text):
             return False
     return True
 
-# number needs to be unicoded
-num = u"608-790-7821"
+# number needs to be unicoded for IDLE
+num0 = u"442-555-1234"
 
-print(is_phone_number(num))
+# This works via Python3 on Solus Linux
+num1 = "442-555-1234"
+
+# This will return false
+not_num = "12-13-2017"
+
+print(is_phone_number(num0))
+print(is_phone_number(num1))
+print(is_phone_number(not_num))
+
+# This will output:
+# True
+# True
+# False
