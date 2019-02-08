09532749b9ddb5dc657dd765fae821a328e53b43
Isaac-Newt <isaac.newt.list@icloud.com>
2019-02-08 09:25:40 -0600

    updates to final review doc

--- School/Final Review/image-with-frame.py
+++ School/Final Review/image-with-frame.py
@@ -1,21 +1,21 @@
-import image 
+import image
 
 def framedImage(original, thickness, red, green, blue):
-    
+
     framedImage = image.EmptyImage(original.getWidth() + (2*thickness), original.getHeight() + (2*thickness))
-    
+
     # Fill in framedImage background with border color
     for x in range(framedImage.getWidth()):
         for y in range(framedImage.getHeight()):
             borderPixel = image.Pixel(red, green, blue)
             framedImage.setPixel(x, y, borderPixel)
-            
+
     # Print the image
     for x in range(thickness + 1, framedImage.getWidth() - thickness):
         for y in range(thickness + 1, framedImage.getHeight() - thickness):
             originalPixel = original.getPixel(x - (thickness +1), y - (thickness + 1))
             framedImage.setPixel(x, y, originalPixel)
-            
+
     # Return the image
     return framedImage
 
