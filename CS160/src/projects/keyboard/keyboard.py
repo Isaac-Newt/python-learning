8fe0d018e1ea04063487f260248246b0adbfbca7
Isaac-Newt <isaac.newt.list@icloud.com>
2019-02-22 09:11:24 -0600

    add zoo and keyboard

---
+++ CS160/src/projects/keyboard/keyboard.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Touchscreen Keyboard
+"""
+
+def spell_check(filename: str) -> None:
+    """Rank words by their proximity to the target"""
+    raise NotImplementedError
+
+
+def main():
+    """Entry point"""
+    spell_check("data/projects/keyboard/sample.in")
+
+
+if __name__ == "__main__":
+    main()
