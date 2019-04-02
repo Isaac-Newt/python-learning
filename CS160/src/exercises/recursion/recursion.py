350ac69c4de91bfaea41cc568d30cee579ecec95
Isaac-Newt <isaac.newt.list@icloud.com>
2019-04-02 08:50:07 -0500

    updates to reucursion exercise, all but gcd working well

--- CS160/src/exercises/recursion/recursion.py
+++ CS160/src/exercises/recursion/recursion.py
@@ -1,84 +1,77 @@
 #!/usr/bin/env python3
-"""Recursion exercise code template"""
+"""
+Recursion exercise
+
+Isaac List - CS160
+April 1, 2019
+"""
 
 
-def gcd(a: int, b: int) -> int:
+def gcd(a_num: int, b_num: int) -> int:
+    """Recursive GCD finder"""
     raise NotImplementedError
 
 
 def hourglass_ite(levels: int) -> None:
-    spaces_count = 0
+    """Iterative hourglass"""
     star_count = levels
     for count in range(levels):
-        stars = "*" * (2 * star_count - 1)
-        spaces = " " * spaces_count
-        print(spaces + stars + spaces)
+        stars = "* " * star_count
+        print(f"{stars:^{levels * 2}}")
         star_count -= 1
-        spaces_count += 1
 
-    star_count = 3
-    spaces_count = ((2 * levels) - star_count) // 2
+    star_count = 2
     for count in range(levels - 1):
-        stars = "*" * star_count
-        spaces = " " * spaces_count
-        print(spaces + stars + spaces)
-        star_count += 2
-        spaces_count -= 1
+        stars = "* " * star_count
+        print(f"{stars:^{levels * 2}}")
+        star_count += 1
 
 
 def diamond_ite(levels: int) -> None:
+    """Iterative Diamond"""
     star_count = 1
-    spaces_count = ((2 * levels) - star_count) // 2
     for count in range(levels):
-        stars = "*" * star_count
-        spaces = " " * spaces_count
-        print(spaces + stars + spaces)
-        star_count += 2
-        spaces_count -= 1
+        stars = "* " * star_count
+        print(f"{stars:^{levels * 2}}")
+        star_count += 1
 
-    spaces_count = 1
     star_count = levels - 1
     for count in range(levels - 1):
-        stars = "*" * (2 * star_count - 1)
-        spaces = " " * spaces_count
-        print(spaces + stars + spaces)
+        stars = "* " * (star_count)
+        print(f"{stars:^{levels * 2}}")
         star_count -= 1
-        spaces_count += 1
 
 
-def up_triangle(spaces_count)
+def hourglass_rec(levels: int, counter: int) -> None:
+    """Recursive Hourglass"""
+    if counter < (2 * levels) - 1:
+        if counter < levels:
+            stars = "* " * (levels - counter)
+        else:
+            stars = "* " * (counter - (levels - 2))
+        print(f"{stars:^{levels * 2}}")
+        hourglass_rec(levels, counter + 1)
 
 
-def hourglass_rec(levels: int) -> None:
-    # spaces_count = 0
-    # star_count = levels
-    # for count in range(levels):
-    #     stars = "*" * (2 * star_count - 1)
-    #     spaces = " " * spaces_count
-    #     print(spaces + stars + spaces)
-    #     star_count -= 1
-    #     spaces_count += 1
-
-    # star_count = 3
-    # spaces_count = ((2 * levels) - star_count) // 2
-    # for count in range(levels - 1):
-    #     stars = "*" * star_count
-    #     spaces = " " * spaces_count
-    #     print(spaces + stars + spaces)
-    #     star_count += 2
-    #     spaces_count -= 1
-
-
-def diamond_rec(levels: int) -> None:
-    raise NotImplementedError
+def diamond_rec(levels: int, counter: int) -> None:
+    """Recursive Diamond"""
+    if counter < (2 * levels) - 1:
+        if counter < levels:
+            stars = "* " * (counter + 1)
+        else:
+            stars = "* " * ((2 * levels - 1) - counter)
+        print(f"{stars:^{levels * 2}}")
+        diamond_rec(levels, counter + 1)
 
 
 def main():
     """Main function"""
     hourglass_ite(5)
-    hourglass_rec(5)
-    # diamond_ite(5)
-    # diamond_rec(5)
+    print()
+    hourglass_rec(5, 0)
+    diamond_ite(5)
+    print()
+    diamond_rec(5, 0)
 
 
 if __name__ == "__main__":
--- CS160/src/exercises/recursion/testground.py
+++ CS160/src/exercises/recursion/testground.py
@@ -13,6 +13,27 @@ def rec_upsideup_triangle(levels, count):
     if count > 0:
         rec_upsideup_triangle(levels, count - 1)
 
+def recursive_hourglass(levels, counter):
+    """Recursive Hourglass"""
+    if counter < (2 * levels) - 1:
+        if counter < levels:
+            stars = "* " * (levels - counter)
+        else:
+            stars = "* " * (counter - (levels - 2))
+        print(f"{stars:^{levels * 2}}")
+        recursive_hourglass(levels, counter + 1)
 
-rec_upsidedown_triangle(7, 7)
-rec_upsideup_triangle(7, 6)
+def recursive_diamond(levels, counter):
+    """Recursive diamond"""
+    if counter < (2 * levels) - 1:
+        if counter < levels:
+            stars = "* " * (counter + 1)
+        else:
+            stars = "* " * ((2 * - 1) - counter)
+        print(f"{stars:^{levels * 2}}")
+        recursive_diamond(levels, counter + 1)
+
+# rec_upsidedown_triangle(7, 7)
+# rec_upsideup_triangle(7, 6)
+
+recursive_diamond(5, 0)
