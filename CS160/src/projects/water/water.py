6c5fba3b9d6aa619edced64e0af71b5efa08123d
Isaac-Newt <isaac.newt.list@icloud.com>
2019-04-16 21:24:28 -0500

    mostly finished water? works, but not quite correctly

--- CS160/src/projects/water/water.py
+++ CS160/src/projects/water/water.py
@@ -3,7 +3,7 @@
 Water jugs project
 
 Isaac List - CS160
-April 15, 2019
+April 16, 2019
 """
 
 
@@ -35,11 +35,11 @@ class State:
         string_1 = f"Jug 1 has {self._jug_1_level} gallons"
         string_2 = f"Jug 2 has {self._jug_2_level} gallons"
         string_3 = f"Alltogether, {self._jug_1_level + self._jug_2_level} gallons"
-        return f"{string_1}, {string_2}. {string_3}."
+        return f"{string_1}, {string_2}. {string_3}.\n"
 
     def clone(self):
         """Copy a state"""
-        return cloned_state = State(self._jug_1_level, self._jug_2_level)
+        return State(self._jug_1_level, self._jug_2_level)
 
     def fill_jug_1(self):
         """Fill jug1 to capacity from the pump"""
@@ -85,9 +85,91 @@ class State:
 
 def search(start_state: object, goal: object, moves_lst: list):
     """Find a sequence of states"""
-    moves.append(start)
+    moves_lst.append(start_state)
 
+    # Fill Jug 1
 
+    # Change the state with each option
+    new_state = start_state.clone()
+    new_state.fill_jug_1()
+    # Decide what to do with the new state
+    if new_state == goal:
+        moves_lst.append(new_state)
+        return
+    elif new_state in moves_lst:
+        pass
+    else:
+        search(new_state, goal, moves_lst)
+
+    # Fill Jug 2
+    
+    # Change the state with each option
+    new_state = start_state.clone()
+    new_state.fill_jug_2()
+    # Decide what to do with the new state
+    if new_state == goal:
+        moves_lst.append(new_state)
+        return
+    elif new_state in moves_lst:
+        pass
+    else:
+        search(new_state, goal, moves_lst)
+
+    # Empty Jug 1
+    
+    # Change the state with each option
+    new_state = start_state.clone()
+    new_state.empty_jug_1()
+    # Decide what to do with the new state
+    if new_state == goal:
+        moves_lst.append(new_state)
+        return
+    elif new_state in moves_lst:
+        pass
+    else:
+        search(new_state, goal, moves_lst)
+
+    # Empty Jug 2
+    
+    # Change the state with each option
+    new_state = start_state.clone()
+    new_state.empty_jug_2()
+    # Decide what to do with the new state
+    if new_state == goal:
+        moves_lst.append(new_state)
+        return
+    elif new_state in moves_lst:
+        pass
+    else:
+        search(new_state, goal, moves_lst)
+
+    # Pour jug 1 into jug 2
+    
+    # Change the state with each option
+    new_state = start_state.clone()
+    new_state.pour_jug_1_to_jug_2()
+    # Decide what to do with the new state
+    if new_state == goal:
+        moves_lst.append(new_state)
+        return
+    elif new_state in moves_lst:
+        pass
+    else:
+        search(new_state, goal, moves_lst)
+
+    # Pour jug 2 into jug 1
+    
+    # Change the state with each option
+    new_state = start_state.clone()
+    new_state.pour_jug_2_to_jug_1()
+    # Decide what to do with the new state
+    if new_state == goal:
+        moves_lst.append(new_state)
+        return
+    elif new_state in moves_lst:
+        pass
+    else:
+        search(new_state, goal, moves_lst)
 
 def main():
     """Main function"""
