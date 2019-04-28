e5c41ae0f80d32838433ea0d44a6487aa4fefad9
Isaac-Newt <isaac.newt.list@icloud.com>
2019-04-28 16:11:17 -0500

    finished heaps exercise

--- CS160/src/exercises/heaps/heaps.py
+++ CS160/src/exercises/heaps/heaps.py
@@ -19,11 +19,11 @@ class BinaryHeapMax:
     def perc_up(self, cur_idx):
         """Moving a node up"""
         while (cur_idx - 1) // 2 >= 0:
-            parent_idx = (cur_idx -1) //2
+            parent_idx = (cur_idx -1) // 2
             if self.heap[cur_idx] > self.heap[parent_idx]:
-                self.swap(cur_idx, parent_idx)
-            else:
-                return
+                temp = self.heap[cur_idx]
+                self.heap[cur_idx] = self.heap[parent_idx]
+                self.heap[parent_idx] = temp
             cur_idx = parent_idx
 
     def perc_down(self, cur_idx):
@@ -31,9 +31,9 @@ class BinaryHeapMax:
         while 2 * cur_idx + 1 <= self.size:
             max_child_idx = self.get_max_child(cur_idx)
             if self.heap[cur_idx] < self.heap[max_child_idx]:
-                self.swap(cur_idx, max_child_idx)
-            else:
-                return
+                temp = self.heap[cur_idx]
+                self.heap[cur_idx] = self.heap[max_child_idx]
+                self.heap[max_child_idx] = temp
             cur_idx = max_child_idx
 
     def insert(self, item):
@@ -41,6 +41,12 @@ class BinaryHeapMax:
         self.heap.append(item)
         self.size += 1
         self.perc_up(self.size - 1)
+        if self.max_size:
+            if self.size > self.max_size:
+                min_idx = self.heap.index(min(self.heap))
+                self.heap.pop(min_idx)
+                self.size -= 1
+
 
     def heapify(self, not_a_heap):
         """Turning a list into a heap"""
@@ -53,7 +59,12 @@ class BinaryHeapMax:
 
     def get_max_child(self, parent_idx):
         """Getting a larger child"""
-        raise NotImplementedError
+        child_left = self.heap[(2 * parent_idx) + 1]
+        child_right = self.heap[(2 * parent_idx) + 2]
+        if child_left > child_right:
+            return child_left
+        else: # child_right >= child_left
+            return child_right
 
     def __len__(self):
         """Get heap size"""
