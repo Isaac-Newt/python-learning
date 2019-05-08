0e0afc5e74fd2d9db50c64e3453d981d8ef2cefa
Isaac-Newt <isaac.newt.list@icloud.com>
2019-05-08 18:16:27 -0500

    it finally works

--- CS160/src/projects/morse/morse.py
+++ CS160/src/projects/morse/morse.py
@@ -1,76 +1,109 @@
-"""Morse code encoding and decoding"""
+"""
+Isaac List - CS160
+May 8, 2019
+
+Morse code encoding and decoding
+"""
 #!/usr/bin/env python3
 # encoding: UTF-8
 
 
-from src.notes.trees.BinaryTree import BinaryTree
-
+# from src.notes.trees.BinaryTree import BinaryTree
+from pythonds3.trees import BinaryTree
 
 class Coder:
     """Morse Code Encoder/Decoder"""
 
     def __init__(self, file_in: str):
         """Constructor"""
-        morse_tree = BinaryTree(None)
+        self.morse_tree = BinaryTree("")
         input_file = open(file_in, "r")
         for line in input_file:
             # Get the line
             line = line.strip()
-            print(line)
 
             # Split into letter and code
             letter_code = line.split()
             letter = letter_code[0]
             code = letter_code[1]
-            current_node = morse_tree
-            for char in code:
-                # Place the letter in its spot based on the code
-                if code.index(char) == (len(code) + 1):
-                    if char == ".":
-                        print("left")
-                        current_node.set_child_left(letter)
-                    elif char == "-":
-                        print("right")
-                        current_node.set_child_right(letter)
-                else:
-                    if char == ".":
-                        print("left")
-                        left = current_node.get_child_left()
-                        if left == None:
-                            morse_tree.set_child_left("")
-                        current_node = current_node.get_child_left()
-                    elif char == "-":
-                        print("right")
-                        right = current_node.get_child_right()
-                        if right == None:
-                            morse_tree.set_child_right("")
-                        current_node = current_node.get_child_right()
+
+            # Add letter to the tree
+            self.follow_and_insert(code, letter)
 
     def follow_and_insert(self, code_str: str, letter: str):
         """Follow the tree and insert a letter"""
-        raise NotImplementedError
+
+        # Set current node to the tree root
+        current_node = self.morse_tree
+
+        # For each character in the code, move to the left or right child
+        for char in code_str:
+            if char == ".":
+                # If child doesn't exist, create an empty node
+                if current_node.get_child_left() == None:
+                    current_node.insert_left("")
+                # Move the current node
+                current_node = current_node.get_child_left()
+            elif char == "-":
+                # If child doesn't exist, create an empty node
+                if current_node.get_child_right() == None:
+                    current_node.insert_right("")
+                # Move the current node
+                current_node = current_node.get_child_right()
+
+        current_node.set_root_val(letter)
 
     def follow_and_retrieve(self, code_str: str):
         """Follow the tree and retrieve a letter"""
-        current_node = morse_tree 
+        current_node = self.morse_tree 
         for char in code_str:
-            if char == ".":
-                print("left")
-                current_node = current_node.get_child_left()
-            if char == "-":
-                print("right")
-                current_node = current_node.get_child_right()
-        return current_node
+            if current_node == None:
+                return None
+            else:
+                if char == ".":
+                    current_node = current_node.get_child_left()
+                if char == "-":
+                    current_node = current_node.get_child_right()
+        if current_node != None:
+            return current_node.get_root_val()
+        else:
+            return None
 
     def find_path(self, tree: object, letter: str, path: str):
         """Find a key"""
-        raise NotImplementedError
+        error = f"Could not encode {letter}: {letter} is not in the tree"
+        left_tree = tree.get_child_left()
+        right_tree = tree.get_child_right()
+
+        if left_tree == None:
+            raise ValueError(error)
+        elif left_tree.get_root_val() == letter:
+            path += "."
+            return path
+        elif right_tree == None:
+            raise ValueError(error)
+        elif right_tree.get_root_val() == letter:
+            path += "-"
+            return path
+        else:
+            left_path = path + "."
+            right_path = path + "-"
+            try:
+                return self.find_path(left_tree, letter, left_path)
+            except:
+                return self.find_path(right_tree, letter, right_path)
+        
 
     def encode(self, msg: str):
         """Encode a message"""
         encoded_string = ""
         for char in msg:
-            find_path(morse_tree, char, path)
+            if char == " ":
+                encoded_string = encoded_string
+            else:
+                path = ""
+                path = self.find_path(self.morse_tree, char, path)
+                encoded_string = encoded_string + path + " "
         return encoded_string
 
     def decode(self, code: str):
@@ -78,7 +111,10 @@ class Coder:
         code_strings = code.split()
         message = ""
         for code in code_strings:
-            letter = follow_and_retrieve(code)
+            letter = self.follow_and_retrieve(code)
+            if letter == None:
+                error = f"Could not decode {code}: {code} is not in the tree"
+                raise ValueError(error)
             message += letter
         return message
 
