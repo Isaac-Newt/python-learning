fb0c096835f1fbbf03d88c7475be31521040d442
Isaac-Newt <isaac.newt.list@icloud.com>
2019-05-09 22:02:30 -0500

    add kevin bacon project

---
+++ CS160/src/projects/kevinbacon/kevinbacon.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Using Breadth first search to find Kevin Bacon's number of actors"""
+
+
+from pythonds3.graphs import Graph
+import sys
+import time
+
+
+def read_file(filename):
+    """Build a graph from the file"""
+    raise NotImplementedError
+
+
+def main():
+    print("---Kevin Bacon number calculator---")
+    print("\nReading the file")
+    b_graph = read_file("data/projects/kevinbacon/movie_actors_full.txt")
+
+
+if __name__ == "__main__":
+    main()
