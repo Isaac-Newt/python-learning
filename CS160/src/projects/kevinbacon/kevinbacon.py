a1bf005155a168fb4ef613dd25888a55054848c2
Isaac-Newt <isaac.newt.list@icloud.com>
2019-05-14 17:12:27 -0500

    It mostly works, but the titles aren't always correct

--- CS160/src/projects/kevinbacon/kevinbacon.py
+++ CS160/src/projects/kevinbacon/kevinbacon.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""Using Breadth first search to find Kevin Bacon's number of actors"""
+"""
+Isaac List - CS160
 
+Using Breadth first search to find Kevin Bacon's number of actors
+"""
 
-from pythonds3.graphs import Graph
 import sys
 import time
+from pythonds3.graphs import Graph
 
 
 def read_file(filename):
@@ -15,18 +18,17 @@ def read_file(filename):
     # Open and read the file
     source = open(filename, "r")
     # Ignore first line
-    line = source.readline() # first line
+    line = source.readline()  # first line
     line = source.readline()
+    # Clean the line
+    line = line.strip()
+    # Break into portions
+    components = line.split("|")
+    title = components[0]
+    actor = components[1]
 
     while line != "":
-        # Clean the line
-        line = line.strip()
-        # Break into portions
-        components = line.split("|")
-        first_title = components[0]
-        title = components[0]
-        actor = components[1]
-
+        first_title = title
         actors = []
         while title == first_title and line != "":
             # Add the actor to the list
@@ -40,11 +42,11 @@ def read_file(filename):
                 actor = components[1]
         # For actors in the list, make connections to other actors
         # If a source/destination doesn't exist, add_edge will add
-        # a new vertex, otherwise will use the existing vertex. 
+        # a new vertex, otherwise will use the existing vertex.
         for actor in actors:
             for other_actor in actors:
                 if other_actor != actor:
-                    kevinbacon_graph.add_edge(actor, other_actor, title)
+                    kevinbacon_graph.add_edge(actor, other_actor, first_title)
 
     # Return the graph
     return kevinbacon_graph
@@ -55,8 +57,8 @@ def find_kevin_bacon_numbers(graph):
     graph.bfs(graph.get_vertex("Kevin Bacon"))
 
 
-def traverse(graph, src, dst):
-    """Traverse a graph"""
+def traverse(dst):
+    """Traverse a graph, find path from dst to Kevin Bacon"""
     path = []
     current = dst
     # Find the actor path
@@ -66,13 +68,14 @@ def traverse(graph, src, dst):
     # Build the path string
     path_strings = []
     for idx in range(1, len(path)):
-        string = f"{path[idx - 1].key} acted with {path[idx].key}"
+        string = f"{path[idx - 1].key} acted with {path[idx].key} in {path[idx - 1].get_neighbor(path[idx])}"
         path_strings.append(string)
     print("\n".join(path_strings))
-    # print(" ".join(vertex.key for vertex in (path)))
 
 
 def main():
+    """Main Function"""
+    # Setup
     print("---Kevin Bacon number calculator---")
     print("\nReading the file")
     b_graph = read_file("data/projects/kevinbacon/movie_actors_full.txt")
@@ -80,10 +83,15 @@ def main():
     # Loop to find number and links for any actor entered
     actor = input("What Actor would you like to trace? (exit to quit): ")
     while actor != "exit":
-        print(f"The Kevin Bacon number for {actor} is {b_graph.get_vertex(actor).get_distance()}.")
-
-        traverse(b_graph, b_graph.get_vertex("Kevin Bacon"), b_graph.get_vertex(actor))
-
+        # Print the Kevin Bacon Number
+        print(
+            f"The Kevin Bacon number for {actor} is {b_graph.get_vertex(actor).get_distance()}."
+        )
+        print()
+        # Print the path from the selected actor to Kevin Bacon
+        traverse(b_graph.get_vertex(actor))
+        print("- - -")
+        # Get a new actor from the user
         actor = input("What Actor would you like to trace? (exit to quit): ")
 
 
